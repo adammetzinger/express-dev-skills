1855300d8fdc78e467c446afd2e68c295adb0372
adammetzinger <adamcm728@gmail.com>
2024-01-03 16:45:19 -0600

    show functionalty

--- controllers/skills.js
+++ controllers/skills.js
@@ -2,7 +2,13 @@ const Skill = require('../models/skills');
 
 module.exports = {
     index,
+    show
 };
+
+function show(req, res) {
+    const skill = Skill.getOne(req.params.id);
+    res.render('skills/show', { skill });
+}
 
 function index(req, res) {
     res.render('skills/index', {
