2a95a399d8d05be78ac33895b23684ec4bb6f8c5
adammetzinger <adamcm728@gmail.com>
2024-01-03 16:18:10 -0600

    finished

--- controllers/skills.js
+++ controllers/skills.js
@@ -2,16 +2,10 @@ const Skill = require('../models/skills');
 
 module.exports = {
     index,
-    show
-}
-
-function show(req, res) {
-    const skill = Skill.getOne(req.params.id);
-    res.render('skills/show', { skill });
-}
+};
 
 function index(req, res) {
-    res.render('todos/index', {
+    res.render('skills/index', {
         skills: Skill.getAll()
     });
 }
--- models/skills.js
+++ models/skills.js
@@ -2,4 +2,13 @@ const skills = [
     {id: 1234, skill: 'JavaScript'},
     {id: 1235, skill: 'CSS'},
     {id: 1236, skill: 'HTML'}
-]
+];
+
+module.exports = {
+    getAll,
+};
+
+
+function getAll() {
+    return skills;
+}
