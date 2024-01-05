6f29f5894c7a8b1e7f4a8d095f8cf83bb03f63a6
adammetzinger <adamcm728@gmail.com>
2024-01-04 19:08:01 -0600

    Lab Part 2 Finish

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,17 +1,57 @@
-const Skill = require('../models/skills');
+const Skill = require('../models/skill');
+
 
 module.exports = {
-    index,
-    show
+  index,
+  show,
+  new: newSkill,
+  create,
+  edit,
+  update,
+  delete: deleteSkill,
 };
 
-function show(req, res) {
-    const skill = Skill.getOne(req.params.id);
-    res.render('skills/show', { skill });
+function deleteSkill(req, res) {
+  Skill.deleteOne(req.params.id);
+  res.redirect('/skills');
+}
+
+function update(req, res) {
+  Skill.update(req.params.id, req.body);
+  res.redirect(`/skills/${req.params.id}`)
+}
+
+function edit(req, res) {
+  const skill = Skill.getOne(req.params.id)
+  res.render('skills/edit', {
+    title: 'Edit Skill',
+    skill
+  })
+}
+
+function create(req, res) {
+  Skill.create(req.body);
+  console.log(req.body)
+  res.redirect('/skills');
+}
+
+function newSkill(req, res) {
+  res.render('skills/new', {
+    title: 'Add New Skill'
+  });
 }
 
 function index(req, res) {
-    res.render('skills/index', {
-        skills: Skill.getAll()
-    });
+  res.render('skills/index', {
+    title: 'I Got Skills!🔥',
+    skills: Skill.getAll()
+  });
+}
+
+function show(req, res) {
+  const skill = Skill.getOne(req.params.id);
+  res.render('skills/show', {
+    title: 'About My Skills',
+    skill
+  });
 }
--- routes/skills.js
+++ routes/skills.js
@@ -3,7 +3,16 @@ var router = express.Router();
 const skillsCtrl = require('../controllers/skills')
 
 /* GET users listing. */
+router.get('/new', skillsCtrl.new)
 router.get('/', skillsCtrl.index);
-router.get('/:id', skillsCtrl.show)
+router.get('/:id', skillsCtrl.show);
+// create /skills
+router.post('/', skillsCtrl.create);
+//delete /skills/:id
+router.get('/:id/edit', skillsCtrl.edit);
+//PUT /skills/:id
+router.put('/:id', skillsCtrl.update);
+
+router.delete('/:id', skillsCtrl.delete);
 
 module.exports = router;
